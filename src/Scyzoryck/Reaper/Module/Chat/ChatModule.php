fa9ca610ef917dbd880eaf50e8891fabb26f305a
scyzoryck <marcin.czarnecki@klubfocus.pl>
2014-08-09 00:01:30 +0200

    added colors and readme

--- src/Scyzoryck/Reaper/Module/Chat/ChatModule.php
+++ src/Scyzoryck/Reaper/Module/Chat/ChatModule.php
@@ -4,54 +4,88 @@ namespace Scyzoryck\Reaper\Module\Chat;
 use Scyzoryck\Reaper\Client\Selector\Filter;
 use Erpk\Harvester\Module\Module;
 use Erpk\Harvester\Exception\InvalidArgumentException;
+use Guzzle\Plugin\Cookie\Cookie;
+use Erpk\Harvester\Client\Client; 
 
 class ChatModule extends Module 
 {
     protected $response;
     
-    protected $erpkChat; 
+    protected $erpkChat = ''; 
     
+    protected $currentRoomId = '';
+    
+    protected $colorCookie;
+    
+    public function __construct(Client $client)
+    {
+        parent::__construct($client);
+        $this->setColor();
+    }
+    
+    public function setCurrentRoomId($id = '')
+    {
+        $this->currentRoomId = $id;
+        return $this;    
+    }
+    
+    public function getCurrentRoomId()
+    {
+        return empty($this->currentRoomId) ? $this->erpkChat['roomId'] : $this->currentRoomId;
+    }
+    
+    public function setColor($color = '2F2F2F')
+    {
+        if(!preg_match("/[0-9a-f]{6}/i", $color))
+            throw new InvalidArgumentException('Invalid color ' . $color . '.');
+        $color = strtoupper($color);
+        if(isset($this->colorCookie))
+        {
+            $this->colorCookie->setValue($color);
+            return $this;
+        }
+        $colorCookie = new Cookie( array( 'name' => 'l_chatroomcolor', 'value' => $color, 'domain' => '.erepublik.com' ) );
+        $this->getSession()->getCookieJar()->add($colorCookie);
+        return $this;
+    }
+
+    public function getColor()
+    {
+        return $this->colorCookie->getValue();
+    }
+     
     public function sendMUMessage($message) 
+    {
+        $this->setCurrentRoomID();
+        $this->sendMessage($message);
+    }
+    
+    public function sendMessage($message) 
     {
         if(empty($message))
             throw new InvalidArgumentException('Empty message');
         $this->getClient()->checkLogin();
-        $this->setErpkChatOptions();
+        if(empty($this->erpkChat))
+            $this->setErpkChatOptions();
+        $token = $this->getSession()->getToken();
+        $roomId = $this->getCurrentRoomId();
         $request = $this->getClient()->post('main/cometchat-room-send');
         $request->addPostFields(
             array( 
-                '_token'      => $this->getSession()->getToken(),
-                'currentroom' => $this->erpkChat['roomId'],
+                '_token'      => $token,
+                'currentroom' => $roomId,
                 'message'     => $message
             )
         );
         $request->setHeader('X-Requested-With', 'XMLHttpRequest');
+        $request->setHeader('Referer', $this->getClient()->getBaseUrl() . 'main/cometchat-chatrooms?roomid=' . $this->getCurrentRoomId() . '&inviteid=' . $this->erpkChat['inviteId'] . '&roomname=' . $this->erpkChat['roomName'] . '&basedata=null');
         $response = $request->send();
     }
     
-    public function setResponse(\Guzzle\Http\Message\Response $response) 
-    {
-        $this->response = $response ;
-    }
-    
-    public function getResponse()
-    {
-        if(!isset($this->response))
-            $this->makeRequest();
-        return $this->response;
-    }
-    
-    protected function makeRequest()
-    {
+    protected function setErpkChatOptions() {
         $request = $this->getClient()->get('');
         $response = $request->send();
-        $this->setResponse($response);
-    }
-
-    protected function setErpkChatOptions() {
-        $this->erpkChat = Filter::extractJSObiect('erpkChat',$this->getResponse());
-        $this->erpkChat['roomName'] = str_replace('=', '', $this->erpkChat['roomName']);
-        $this->erpkChat['inviteId'] = md5(''); //fuck this shit. 
+        $this->erpkChat = Filter::extractJSObiect('erpkChat',$response);
     }
 
 }
